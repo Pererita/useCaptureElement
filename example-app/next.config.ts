2ef920dbe754135a22de09a15c2cf10afe99cbc7
Jesús Perera <jesus2perera3@gmail.com>
2026-03-08 00:43:58 -0400

    chore: Update `next.config.ts` with no functional changes.

--- example-app/next.config.ts
+++ example-app/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/useCaptureElement",
-  trailingSlash: true, 
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
