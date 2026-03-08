e3babd84ae121786edf19a6687696c0ca1ebfb03
Jesús Perera <jesus2perera3@gmail.com>
2026-03-08 00:08:20 -0400

    fix: Adjust build script to create `.nojekyll` and disable `assetPrefix` in Next.js configuration for GitHub Pages deployment.

--- example-app/next.config.ts
+++ example-app/next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export",
   basePath: "/useCaptureElement",
-  assetPrefix: "/useCaptureElement/",
+  //assetPrefix: "/useCaptureElement/",
   trailingSlash: true,
   images: {
     unoptimized: true,
